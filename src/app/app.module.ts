725fbdfb582661ddd1bf71f0f9e1e0631ed2a8c2
donkagunila <donkagunila@gmail.com>
2019-05-15 07:02:38 +0300

    initial structure setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './_auth/login/login.component';
+import { RegisterComponent } from './_auth/register/register.component';
+import { ForgetComponent } from './_auth/forget/forget.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { FooterComponent } from './_shared/footer/footer.component';
+import { HeaderComponent } from './_shared/header/header.component';
+import { NavbarComponent } from './pages/subs/navbar/navbar.component';
+import { SidebarComponent } from './pages/subs/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgetComponent,
+    HomeComponent,
+    ProfileComponent,
+    DashboardComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
