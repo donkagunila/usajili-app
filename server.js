fba5996d4aa4c98982fcc51d5f1908ab019b483b
donkagunila <donkagunila@gmail.com>
2019-05-22 10:32:19 +0300

    the master setup

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/usajili'));
+app.use(express.static(__dirname + '/dist/usajili'));
 
 app.get('/*', function(req,res) {
     
