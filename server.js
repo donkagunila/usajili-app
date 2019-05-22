4acb27db6ea5232136f9a41f124e8f98735b323e
donkagunila <donkagunila@gmail.com>
2019-05-22 10:41:32 +0300

    the master setup

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist/usajili'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname + '/dist/index.html'));
+res.sendFile(path.join(__dirname + '/dist/usajili/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
